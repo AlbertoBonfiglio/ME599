70a040344eb32ea386fcde0ecd959d63934a3f4b
alberto.bonfiglio@gmail.com <R0b0tics!!152>
2016-01-28 14:58:31 -0800

    Lab 3 sort complete

--- Lab3/summer.py
+++ Lab3/summer.py
@@ -4,31 +4,64 @@ import matplotlib.pyplot as plt
 from matplotlib.pylab import figtext
 import numpy as np
 from Lab3.classes.utils import *
+from time import clock, process_time, time, perf_counter
 
 
+timers = [time, clock, process_time, perf_counter] #different timing functions
+listlen = [1, 10, 100, 1000, 10000, 100000, 1000000]
 
 
-def draw_plot(states, times, caption):
+def draw_plot(values, caption):
     try:
+        t_groups = len(timers)
+        n_groups = len(listlen)
+
+        pivots = []
+        for n in range(t_groups):
+            pivots.append([value[n] for value in values])
+
+        print(pivots)
+
         with plt.xkcd():
             #plt.style.use('fivethirtyeight')
-            fig, ax = plt.subplots(nrows=1, ncols=1, sharex=True, sharey=True)
+
+            fig, axes = plt.subplots(nrows=2, ncols=4, sharex=True, sharey=True)
             fig.suptitle('Spring damping over time',  fontsize=18, fontweight='bold')
 
-            displacement = [n[0] for n in states]
-            ax.plot(times, displacement)
+            for n in range(t_groups):
+                axes[0,0].plot(listlen, pivots[n], color=np.random.rand(3,1))
 
-            ax.set_xlabel('Time (s)')
-            ax.set_ylabel('Displacement (m)')
-            ax.axis('tight')
-            figtext(.52, .15, caption)
+
+
+
+
+            position = np.arange(n_groups)
+            width=0
+            for n in range(t_groups):
+                axes[0,1].bar(np.arange(n_groups) + width, pivots[n], width=0.25, color=np.random.rand(3,1))
+                width +=.25
+
+            axes[0,1].set_xlabel('Time (s)')
+            axes[0,0].set_ylabel('Displacement (m)')
+
+            axes[0,1].set_xticks(position + (width/t_groups) *2)
+            #xes[0,1].set_xticklabels(timers)
+
+            axes[0,1].axis('tight')
+            #figtext(.52, .15, caption)
+
+
+
+
+
+
             plt.show()
 
     except Exception as ex:
         print(ex)
 
 
-def get_lists(listlenghts):
+def get_randomlists(listlenghts):
     retval = []
     for n in listlenghts:
         retval.append(np.random.uniform(-1.0, 1.0, size=n ))
@@ -36,10 +69,40 @@ def get_lists(listlenghts):
     return retval
 
 
-def main(argv):
-    listlen = [1, 10, 100, 1000, 10000, 100000, 1000000]
+def time_list_sort(lists):
+    retval=[]
     try:
-        lists = get_lists(listlen)
+        for n in lists:
+            retval.append(timethis(sorted, n))
+
+        return retval
+    except Exception as ex:
+        print(ex)
+
+
+def timethis(func, list, epochs=10):
+    retval = []
+    for timer in timers:
+        print('Processing function {0}'.format(timer))
+        tempval = 0
+        for n in range(epochs):
+            t1 = timer()
+            func(list)
+            tempval += (timer() - t1)
+
+        tempval = tempval/epochs
+
+        retval.append(tempval)
+
+    return retval
+
+
+def main(argv):
+    try:
+
+        times = time_list_sort(get_randomlists(listlen))
+
+        draw_plot(times, 'blah')
 
     except Exception as ex:
         print(ex)
